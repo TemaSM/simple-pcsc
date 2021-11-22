8d34eb66305aa89e4357a9d6c47039ffd03bcab9
Michael <clonex_kontakt@hotmail.com>
2021-11-22 02:23:21 +0100

    Fixed new Buffer() deprecated errors

--- src/Reader.js
+++ src/Reader.js
@@ -67,7 +67,7 @@ class Reader extends EventEmitter {
 	}
 
 	static reverseBuffer(src) {
-		let buffer = new Buffer(src.length);
+		let buffer = Buffer.alloc(src.length);
 		for (var i = 0, j = src.length - 1; i <= j; ++i, --j) {
 			buffer[i] = src[j];
 			buffer[j] = src[i];
@@ -92,7 +92,7 @@ class Reader extends EventEmitter {
 
 	getTagUid(protocol) {
 
-		let packet = new Buffer([
+		let packet = Buffer.from([
 			0xFF, // Class
 			0xCA, // Ins
 			0x00, // P1: Get current card UID
